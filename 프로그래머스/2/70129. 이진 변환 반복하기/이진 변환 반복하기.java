8fcda4829719ed37646216603da6ab1937b3e446
CDANIEL0123 <68948404+CDANIEL0123@users.noreply.github.com>
2025-10-23 00:59:36 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 714.06 ms, Memory: 391 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -2,25 +2,26 @@ class Solution {
     public int[] solution(String s) {
         int count_zero = 0;
         int count_while = 0;
-        String binary = s;
-        
-        while(!binary.equals("1")){
-            count_while +=1;
-            String non_zero = "";
-            for(int i =0; i<binary.length(); i++){
-            if(binary.charAt(i) == '1'){
-                non_zero += binary.charAt(i);
-            }else{
-                count_zero +=1;
-            }
-        }
-
-        binary = Integer.toBinaryString(non_zero.length());
+        int[] answer = new int[2];
+       
+        while (!s.equals("1")){
+            count_while ++;
+            String del_zero = "";
             
+            for(int i =0; i<s.length(); i++){
+                if(s.charAt(i) =='1'){
+                    del_zero += "1";
+                } else{
+                    count_zero ++;
+                }
+            }
+            s = Integer.toBinaryString(del_zero.length());
         }
         
-        int[] answer = {count_while, count_zero};
-        
+        answer[0] = count_while;
+        answer[1] = count_zero;
+
         return answer;
     }
+
 }
