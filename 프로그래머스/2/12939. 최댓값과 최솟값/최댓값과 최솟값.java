9751880f23a54e690bbfae6413a5e4da318343f0
CDANIEL0123 <68948404+CDANIEL0123@users.noreply.github.com>
2025-10-22 23:24:56 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 13.49 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,20 +1,26 @@
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        String[] arrn = s.split(" ");
-        int max_num = Integer.parseInt(arrn[0]);
-        int min_num = Integer.parseInt(arrn[0]);
+        String answer ="";
+        //s = s.replaceAll(" ","");
         
-        for(int i =0; i<arrn.length; i++){
-            int num = Integer.parseInt(arrn[i]);
-            if (num<min_num){
-                min_num = num;
-            }
-            else if (num > max_num){
-                max_num = num;
-            }
+        String[] tokens = s.trim().split(" ");
+        
+        int min_num = Integer.parseInt(tokens[0]);
+        int max_num = Integer.parseInt(tokens[0]);
+        
+        for(int i=0; i<tokens.length; i++){
+            int current = Integer.parseInt(tokens[i]);
+            
+            if ( current > max_num)
+                max_num = current ;
+            
+            if (current < min_num)
+                min_num = current ;
+
         }
-        answer += min_num + " " + max_num;
+        answer += min_num;
+        answer += " ";
+        answer += max_num;
         return answer;
     }
 }
