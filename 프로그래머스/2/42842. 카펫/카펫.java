04655403472d92c6d5c63a20b592f2a88f9c1b2d
CDANIEL0123 <68948404+CDANIEL0123@users.noreply.github.com>
2025-10-26 22:29:58 +0900

    [level 2] Title: 카펫, Time: 4.85 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,20 +2,19 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         int sum = brown + yellow;
+        int ga =0;
+        int se =0;
         
-        for (int i=1; i<sum/2; i++){
-            int j = sum/i;
-            if(yellow == (j-2) * (i-2)){
-                if(j>i){
-                    answer[0] = j;
-                    answer[1] = i;
-                }else{
-                    answer[0] = i;
-                    answer[1] = j;
-                }
+        for(int i=(sum/2); i>0; i--){
+            ga = i;
+            se = sum/i;
+            if( (ga-2) * (se-2) == yellow){
+                break;
             }
         }
         
+        answer[0] = ga;
+        answer[1] = se;
         
         return answer;
     }
