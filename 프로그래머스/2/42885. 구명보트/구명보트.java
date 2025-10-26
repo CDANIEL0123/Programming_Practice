bcd8129edd3403048f538a2bffb5d97e96418e6a
CDANIEL0123 <68948404+CDANIEL0123@users.noreply.github.com>
2025-10-26 23:05:52 +0900

    [level 2] Title: 구명보트, Time: 14.19 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,23 +3,18 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        boolean[] save = new boolean[people.length];
-        int pl = people.length;
-        int j_limit = 0;
         Arrays.sort(people);
+        int sum = 0;
+        int left = 0;
+        int right = people.length - 1;
         
-        for(int i =pl-1; i>=0; i--){
-            if(!save[i]){
-                int total = people[i];
-                answer++;
-                save[i] = true;
-                if(total+people[j_limit] <= limit && !save[j_limit]){
-                    save[j_limit] = true;
-                    j_limit +=1;
-                }
-            }   
+        while (left  <= right) {
+            answer +=1;
+            if(people[right]+people[left] <= limit){
+                left ++;
+            }
+            right --;
         }
-
         return answer;
     }
 }
