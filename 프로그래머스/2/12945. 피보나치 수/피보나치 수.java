fa8ad4df245977afdd2bc1e69b5ceabd7c5264ad
CDANIEL0123 <68948404+CDANIEL0123@users.noreply.github.com>
2025-10-26 22:23:44 +0900

    [level 2] Title: 피보나치 수, Time: 2.50 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -7,7 +7,8 @@ class Solution {
         fibo[1] = 1;
         
         for(int i = 2; i<n+1; i++){
-            fibo[i] = (fibo[i-1] + fibo[i-2]) % 1234567;
+            fibo[i] = (fibo[i-1] + fibo[i-2]) % 1234567; 
+            //int 범위를 넘는 숫자가 들어가 오버플로우가 발생할 수 있으니 즉시 연산해줘야
         }
         
         answer = fibo[n];
