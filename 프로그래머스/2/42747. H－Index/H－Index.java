33294f6599e02cc920cf2eeef81b9d16d623aa3e
CDANIEL0123 <68948404+CDANIEL0123@users.noreply.github.com>
2025-10-28 19:44:51 +0900

    [level 2] Title: H-Index, Time: 6.08 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,16 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
         Arrays.sort(citations);
-        int cl = citations.length;
-        
-        for(int i =cl-1; i>=0; i--){
-            int indexh = cl-i;
-            if(indexh <= citations[i]){
-                answer = indexh;
+        int answer = 0;
+        int ci_len = citations.length;
+        int pointer = 0;
+
+        // 0 1 3 5 6
+        while (answer <= ci_len+1){
+            int counter = 0;
+            
+            for(int i : citations){
+                if (i >= answer ){
+                    counter ++;
+                } else{
+                    
+                }
+            }
+            if(counter >= answer){
+                answer ++;
+            }else{
+                answer --;
+                break;
             }
         }
+        
         
         
         return answer;
